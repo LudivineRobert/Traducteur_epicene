9796b8051c76b0a733403e5459e8089c55a192cf
psawa <mkbloody@gmail.com>
2019-11-15 08:38:17 +0100

    file dedicated to get the relationships

--- get_relations.py
+++ get_relations.py
@@ -10,11 +10,12 @@ import spacy
 
 nlp = spacy.load('fr_core_news_sm')
 
-#text = ("Ni le publiciste que j'ai rencontré hier, ni le journaliste, ni le tribun, ni l'orateur, ni le conférencier ne sont aujourd'hui de simples citoyens.")
+#text = ("Ni l.e.a publiciste que j'ai rencontré.e hier, ni le journaliste, ni le tribun, ni l'orateur, ni le conférencier ne sont aujourd'hui de simples citoyens.")
 #text = ("j'ai mangé le pomme")
 #text = ("j'ai mangé le pomme. la pomme que j'ai mangée. la tortue que j'ai adoptée. la ville où je suis allé.")
-text = ("la ville où je suis allé. la pomme que j'ai mangée. les beaux enfants jouent dans le parc.")
+#text = ("la ville où je suis allé. la pomme que j'ai mangée. Les grands bâtiments sont habités par des vieux.")
 #text = ("j'ai mangé la pomme rouge.")
+text = ("L'auteur que j'ai rencontré était intéressant.")
 doc = nlp(text)
 
 # whole syntax tree
@@ -22,28 +23,32 @@ for token in doc:
     print(token.text, token.dep_, token.head.text, token.head.pos_, [child for child in token.children])
 
 # what we need
-def get_adj(): # NO appos:nmod
+def get_adj(doc): # NO appos:nmod
     '''Takes doc as input and returns a list of 2-tuples with ADJ, and related noun'''
-    return [(token.text, token.head.text) for token in doc if token.dep_ == "amod" or token.dep_ == "nmod"]
+    return [(token.i, token.text, token.head.text) for token in doc if token.dep_ == "amod" or token.dep_ == "nmod"]
 
-def get_relative_clauses():
+def get_relative_clauses(doc):
     '''Takes doc as input and returns a list of 3-tuples
     containing a word-form (VERB), related noun (acl:relcl)
     and a list containing inflected avoir or empty list if AUX = etre'''
-    return [(token.text,
-             token.head.text,
-             [child for child in token.children if child.lemma_ == "avoir"]) for token in doc if token.dep_ == "acl:relcl"]
+    return [(token.i, token.text, token.head.text, [child for child in token.children if child.lemma_ == "avoir"]) for token in doc if token.dep_ == "acl:relcl"]
 
 #def get_aux(text): # I consider copulas and auxiliaries be part of same category
 #    '''Takes a text as input and returns a list of 4-tuples with word-form, lemma, relation and related noun, only if word-form is AUX'''
 #    return [(token.text, token.lemma_, token.head.text, [str(child) for child in token.children]) for token in doc if token.dep_ == "aux" or token.dep_ == "cop"]
 
-adj = get_adj()
-rel_clause = get_relative_clauses()
+def get_index_of_verbs_to_epicenize(doc):
+    clauses = get_relative_clauses(doc)
+    index_list = []
+    #pdb.set_trace()
+    for i in clauses:
+        index_list.append(i[0])
+    return index_list
 
-print(rel_clause)
-print(adj)
-
-for relation in rel_clause: # prints the name related to "avoir" that must be handled somehow
-    if relation[2] != []:
-        print(relation[1])
+def get_index_of_adjectives_to_epicenize(doc):
+    clauses = get_adj(doc)
+    index_list = []
+    #pdb.set_trace()
+    for i in clauses:
+        index_list.append(i[0])
+    return index_list
