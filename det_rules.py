4ac496676b29a9616035eee1cbccae6f382bfc6d
Ludivine_Robert <ludivinerobert3@gmail.com>
2019-11-18 11:23:06 +0100

    +pronouns

--- det_rules.py
+++ det_rules.py
@@ -25,7 +25,7 @@ text= "le publiciste"
 #text= "divers publicistes"
 #text= "différents publiciste"
 
-
+#determinant part
 defini= re.sub(r"\b([Ll]e|[Ll]a)\b","l•e•a",text)
 print(defini)
 
@@ -35,17 +35,17 @@ print(indefini)
 partitif= re.sub(r"\b(du|de l'|de la)\b","d•u•e la",text)
 print(partitif)
 
-demonstratif= re.sub(r"\b(ce|cet|cette)\b","ce•tte",text)
-print(demonstratif)
+ddemonstratif= re.sub(r"\b(ce|cet|cette)\b","ce•tte",text)
+print(ddemonstratif)
 
-possessif1= re.sub(r"\b(mon|ma)\b","m•on•a",text)
-print(possessif1)
+dpossessif1= re.sub(r"\b(mon|ma)\b","m•on•a",text)
+print(dpossessif1)
 
-possessif2= re.sub(r"\b(ton|ta)\b","t•on•a",text)
-print(possessif2)
+dpossessif2= re.sub(r"\b(ton|ta)\b","t•on•a",text)
+print(dpossessif2)
 
-possessif3= re.sub(r"\b(son|sa)\b","s•on•a",text)
-print(possessif3)
+dpossessif3= re.sub(r"\b(son|sa)\b","s•on•a",text)
+print(dpossessif3)
 
 interrogatifsg= re.sub(r"\b(quel|quelle)\b","quel•le",text)
 print(interrogatifsg)
@@ -65,8 +65,8 @@ print(indefnegatifsg2)
 indefnegatifpl2= re.sub(r"\b(nuls|nulles)\b","nul•le•s",text)
 print(indefnegatifpl2)
 
-indeftotalite= re.sub(r"\b(tout|toute)\b","tout•e•s",text)
-print(indeftotalite)
+dindeftotalite= re.sub(r"\b(tout|toute)\b","tout•e•s",text)
+print(dindeftotalite)
 
 indefpluralite1= re.sub(r"\b(certains|certaines)\b","certain•e•s",text)
 print(indefpluralite1)
@@ -76,3 +76,41 @@ print(indefpluralite2)
 
 indefpluralite3= re.sub(r"\b(différents|diférentes)\b","différent•e•s",text)
 print(indefpluralite3)
+
+#pronouns part
+pronomsg= re.sub(r"\b(il|elle)\b","iel",text)
+print(pronomsg)
+
+pronompl= re.sub(r"\b(ils|elles)\b","iels",text)
+print(pronompl)
+
+pdemonstratif= re.sub(r"\b(celui|celle)\b","cel•ui•le",text)
+print(pdemonstratif)
+
+ppossessif1= re.sub(r"\b(le mien|la mienne)\b","l•e•a mien•ne",text)
+print(ppossessif1)
+
+ppossessif2= re.sub(r"\b(le tien|la tienne)\b","l•e•a tien•ne",text)
+print(ppossessif2)
+
+ppossessif3= re.sub(r"\b(le sien|la sienne)\b","l•e•a sien•ne",text)
+print(ppossessif3)
+
+ppossessif4= re.sub(r"\b(le nôtre|la nôtre)\b","l•e•a nôtre",text)
+print(ppossessif4)
+
+ppossessif5= re.sub(r"\b(le vôtre|la vôtre)\b","l•e•a vôtre",text)
+print(ppossessif5)
+
+ppossessif6= re.sub(r"\b(le leur|la leur)\b","l•e•a leur",text)
+print(ppossessif6)
+
+pindefsingularite= re.sub(r"\b(chacun|chacune)\b","chacun•e",text)
+print(pindefsingularite)
+
+pindefpluralite= re.sub(r"\b(certains|certaines)\b","certain•e•s",text)
+print(pindefpluralite)
+
+pindeftotalite= re.sub(r"\b(tous|toutes)\b","tou•te•s",text)
+print(pindeftotalite)
+
