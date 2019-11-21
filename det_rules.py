b828d6091c80f381ab1414b0bd64563e9e4bef2f
psawa <mkbloody@gmail.com>
2019-11-21 11:17:35 +0100

    Update det rules

--- det_rules.py
+++ det_rules.py
@@ -7,112 +7,71 @@ Created on Wed Nov 13 11:21:01 2019
 """
 import re
 
-text= "le publiciste"
-#text= "un publiciste"
-#text= "du publiciste"
-#text= "ce publiciste"
-#text= "mon publiciste"
-#text= "ton publiciste"
-#text= "son publiciste"
-#text= "quel publiciste"
-#text= "quels publicistes"
-#text= "aucun publiciste"
-#text= "aucuns publicistes"
-#text= "nul publiciste"
-#text= "nuls publicistes"
-#text= "tout publicistes"
-#text= "certains publicistes"
-#text= "divers publicistes"
-#text= "différents publiciste"
-#text= "il"
-#text= "elles"
-#text= "la mienne"
+def epcienize_det(det):
+    #determinant part
+    
+    #defini
+    det = re.sub(r"\b(le|la)\b","l·e·a",det)
+    #indefini
+    det = re.sub(r"\b(un|une)\b","un·e",det)
+    #partitif
+    det = re.sub(r"\b(du|de l'|de la)\b","d·u·e la",det)    
+    #ddemonstratif
+    det = re.sub(r"\b(ce|cet|cette)\b","ce·tte",det)
+    #dpossessif1    
+    det = re.sub(r"\b(mon|ma)\b","m·on·a",det) 
+    #dpossessif2
+    det = re.sub(r"\b(ton|ta)\b","t·on·a",det)  
+    #dpossessif3
+    det = re.sub(r"\b(son|sa)\b","s·on·a",det) 
+    #interrogatifsg
+    det = re.sub(r"\b(quel|quelle)\b","quel·le",det)
+    #interrogatifpl
+    det = re.sub(r"\b(quels|quelles)\b","quel·le·s",det)   
+    #indefnegatifsg1
+    det = re.sub(r"\b(aucun|aucune)\b","aucun·e",det)    
+    #indefnegatifpl1
+    det = re.sub(r"\b(aucuns|aucunes)\b","aucun·e·s",det)
+    #indefnegatifsg2    
+    det = re.sub(r"\b(nul|nulle)\b","nul·le",det)
+    #indefnegatifpl2    
+    det = re.sub(r"\bnul(s|es)\b","nul·le·s",det) 
+    #dindeftotalite
+    det = re.sub(r"\b(tout|toutes)\b","tout·e·s",det) 
+    #indefpluralite1
+    det = re.sub(r"\bcertain(s|es)\b","certain·e·s",det)
+    #indefpluralite2    
+    det = re.sub(r"\bdiver(s|ses)\b","divers·es",det) 
+    #indefpluralite3
+    det = re.sub(r"\bdifférent(s|es)\b","différent·e·s",det)
+    
+    return det
 
-#determinant part
-defini= re.sub(r"\b(le|la)\b","l·e·a",text)
-print(defini)
-
-indefini= re.sub(r"\b(un|une)\b","un·e",text)
-print(indefini)
-
-partitif= re.sub(r"\b(du|de l'|de la)\b","d·u·e la",text)
-print(partitif)
-
-ddemonstratif= re.sub(r"\b(ce|cet|cette)\b","ce·tte",text)
-print(ddemonstratif)
-
-dpossessif1= re.sub(r"\b(mon|ma)\b","m·on·a",text)
-print(dpossessif1)
-
-dpossessif2= re.sub(r"\b(ton|ta)\b","t·on·a",text)
-print(dpossessif2)
-
-dpossessif3= re.sub(r"\b(son|sa)\b","s·on·a",text)
-print(dpossessif3)
-
-interrogatifsg= re.sub(r"\b(quel|quelle)\b","quel·le",text)
-print(interrogatifsg)
-
-interrogatifpl= re.sub(r"\b(quels|quelles)\b","quel·le·s",text)
-print(interrogatifpl)
-
-indefnegatifsg1= re.sub(r"\b(aucun|aucune)\b","aucun·e",text)
-print(indefnegatifsg1)
-
-indefnegatifpl1= re.sub(r"\b(aucuns|aucunes)\b","aucun·e·s",text)
-print(indefnegatifpl1)
-
-indefnegatifsg2= re.sub(r"\b(nul|nulle)\b","nul·le",text)
-print(indefnegatifsg2)
-
-indefnegatifpl2= re.sub(r"\bnul(s|es)\b","nul·le·s",text)
-print(indefnegatifpl2)
-
-dindeftotalite= re.sub(r"\b(tout|toutes)\b","tout·e·s",text)
-print(dindeftotalite)
-
-indefpluralite1= re.sub(r"\bcertain(s|es)\b","certain·e·s",text)
-print(indefpluralite1)
-
-indefpluralite2= re.sub(r"\bdiver(s|ses)\b","divers·es",text)
-print(indefpluralite2)
-
-indefpluralite3= re.sub(r"\bdifférent(s|es)\b","différent·e·s",text)
-print(indefpluralite3)
-
-#pronouns part
-pronomsg= re.sub(r"\b(il|elle)\b","iel",text)
-print(pronomsg)
-
-pronompl= re.sub(r"\b(ils|elles)\b","iels",text)
-print(pronompl)
-
-pdemonstratif= re.sub(r"\b(celui|celle)\b","cel·ui·le",text)
-print(pdemonstratif)
-
-ppossessif1= re.sub(r"\b(le\smien|la\smienne)\b","l·e·a mien·ne",text)
-print(ppossessif1)
-
-ppossessif2= re.sub(r"\b(le\stien|la\stienne)\b","l·e·a tien·ne",text)
-print(ppossessif2)
-
-ppossessif3= re.sub(r"\b(le\ssien|la\ssienne)\b","l·e·a sien·ne",text)
-print(ppossessif3)
-
-ppossessif4= re.sub(r"\b(le\snôtre|la\snôtre)\b","l·e·a nôtre",text)
-print(ppossessif4)
-
-ppossessif5= re.sub(r"\b(le\svôtre|la\svôtre)\b","l·e·a vôtre",text)
-print(ppossessif5)
-
-ppossessif6= re.sub(r"\b(le\sleur|la\sleur)b","l·e·a leur",text)
-print(ppossessif6)
-
-pindefsingularite= re.sub(r"\b(chacun|chacune)\b","chacun·e",text)
-print(pindefsingularite)
-
-pindefpluralite= re.sub(r"\b(certains|certaines)\b","certain·e·s",text)
-print(pindefpluralite)
-
-pindeftotalite= re.sub(r"\b(tous|toutes)\b","tou·te·s",text)
-print(pindeftotalite)
+def epicenize_pron(pron):
+    #pronouns part
+    #pronomsg
+    pron = re.sub(r"\b(il|elle)\b","iel",pron)    
+    #pronompl
+    pron = re.sub(r"\b(ils|elles)\b","iels",pron)
+    #pdemonstratif
+    pron = re.sub(r"\b(celui|celle)\b","cel·ui·le",pron)    
+    #ppossessif1
+    pron = re.sub(r"\b(le\smien|la\smienne)\b","l·e·a mien·ne",pron) 
+    #ppossessif2
+    pron = re.sub(r"\b(le\stien|la\stienne)\b","l·e·a tien·ne",pron)
+    #ppossessif3    
+    pron = re.sub(r"\b(le\ssien|la\ssienne)\b","l·e·a sien·ne",pron) 
+    #ppossessif4
+    pron = re.sub(r"\b(le\snôtre|la\snôtre)\b","l·e·a nôtre",pron)  
+    #ppossessif5
+    pron = re.sub(r"\b(le\svôtre|la\svôtre)\b","l·e·a vôtre",pron)    
+    #ppossessif6
+    pron = re.sub(r"\b(le\sleur|la\sleur)b","l·e·a leur",pron)
+    #pindefsingularite    
+    pron = re.sub(r"\b(chacun|chacune)\b","chacun·e",pron)
+    #pindefpluralite    
+    pron = re.sub(r"\b(certains|certaines)\b","certain·e·s",pron)
+    #pindeftotalite    
+    pron = re.sub(r"\b(tous|toutes)\b","tou·te·s",pron) 
+    
+    return pron
