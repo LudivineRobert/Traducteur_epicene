c6d29ccb6d66e6e35e523f8fda59665bcf598b66
Khaams <Xnotmynames@gmail.com>
2019-11-23 14:35:07 +0100

    Here's the updated files to check human beingness
    Now take care of words which is not in the first "LITERAL" but also all hyperonyms tags.

--- Primitives.py
+++ Primitives.py
@@ -13,8 +13,10 @@ root = tree.getroot()
 #First : find the word in the xml " >WORD</LITERAL> "
 # Second : Find the hypernym and go to the hyperonyms
 #Third : go to the upper Hypernym : physical existence or not, and HUMAN
-
-
+"""
+for element in root.iter("another", "child"):
+...     print("%s - %s" % (element.tag, element.text))
+"""
 def get_lexical_entry(lemma):
     """
     lemma is the lexical word
@@ -23,6 +25,11 @@ def get_lexical_entry(lemma):
     for element in tree.findall('./SYNSET'):
         if lemma == element.find('./SYNONYM/LITERAL').text:
             return element
+    
+    for element in tree.findall('./SYNSET'):
+        for elmt in element.findall('./SYNONYM/LITERAL'):
+            if lemma == elmt.text:
+                return element
     return None
 
 test = get_lexical_entry('bouteille')
@@ -33,8 +40,12 @@ def get_hyperonym(lexical_entry):
     Lexical_entry is the adress in the tree
     Get the hyperonym of the element
     """
-    if lexical_entry != None:   
-        return lexical_entry.find('./ILR[@type="hypernym"]').text
+    if lexical_entry != None:
+        if lexical_entry.find('./ILR[@type="hypernym"]') != None:   
+            return lexical_entry.find('./ILR[@type="hypernym"]').text
+        else:
+            return lexical_entry.find('./ILR[@type="instance_hypernym"]').text
+        
     return None
 
 test_h = get_hyperonym(test)
@@ -65,7 +76,8 @@ def from_primitive_to_lemma(Primitive):
     if Primitive != None:
         return Primitive.find('./SYNONYM/LITERAL').text
     return None
-    
+    #Btw, attention au différent LITERAL si il apparait en deuxième ou troisième position, il n'est pas pris en compte
+
 
 def looking_for_human(word):
     """
@@ -87,7 +99,7 @@ def looking_for_human(word):
 #===============================Test============================================
 
 from_primitive_to_lemma(find_primitive(test_h))
-Noun_list = ['étudiant', 'boulanger','bouteille','verre','chèvre','souris']
+Noun_list = ['étudiant','chanteur','mage','magicien','lapin','carotte','magie','chapeau']
 
 for Noun in Noun_list :
     print('Voici le nom traité :', Noun)
