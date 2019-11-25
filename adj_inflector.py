cc5e11834426036a449745edab7846b90ed600ca
psawa <mkbloody@gmail.com>
2019-11-25 11:46:42 +0100

    Adjective inflector working, for gender and number

---
+++ adj_inflector.py
@@ -0,0 +1,102 @@
+import xml.etree.ElementTree as ET
+import pdb
+
+tree = ET.parse('Morphalou/adjective_Morphalou3.1_LMF.xml')
+root = tree.getroot()
+
+
+def get_lexical_entry(orthography):
+    #pdb.set_trace()
+    for element in tree.findall('./lexicalEntry'):
+        for ortho in element.findall('./formSet/inflectedForm/orthography'):
+            #pdb.set_trace()
+            if ortho.text == orthography:
+                return element
+    raise ValueError('Lexical entry not found')
+
+def get_gender(orthography):
+    lexical_entry = get_lexical_entry(orthography)
+    if lexical_entry != None:
+        #pdb.set_trace()
+        for inflexion in lexical_entry.findall('./formSet/inflectedForm'):
+            #pdb.set_trace()
+            if inflexion.find('./orthography').text == orthography:
+                return inflexion.find('./grammaticalGender').text
+    raise ValueError('Word not found')
+
+def get_feminine(orthography):
+    if get_gender(orthography) == 'feminine':
+        print('Adjective already feminine')
+        return None
+    lexical_entry = get_lexical_entry(orthography)
+    if lexical_entry != None:
+        for inflexion in lexical_entry.findall('./formSet/inflectedForm'):
+            gender = inflexion.find('./grammaticalGender').text
+            if gender == 'invariable':
+                print('Epicene adjective')
+                return None
+            if gender == 'feminine':
+                return inflexion.find('./orthography').text
+
+def get_masculine(orthography):
+    if get_gender(orthography) == 'masculine':
+        print('Adjective already masculine')
+        return None
+    lexical_entry = get_lexical_entry(orthography)
+    if lexical_entry != None:
+        for inflexion in lexical_entry.findall('./formSet/inflectedForm'):
+            gender = inflexion.find('./grammaticalGender').text
+            if gender == 'invariable':
+                print('Epicene adjective')
+                return None
+            if gender == 'masculine':
+                return inflexion.find('./orthography').text
+            
+            
+            
+def get_number(orthography):
+    lexical_entry = get_lexical_entry(orthography)
+    if lexical_entry != None:
+        #pdb.set_trace()
+        for inflexion in lexical_entry.findall('./formSet/inflectedForm'):
+            #pdb.set_trace()
+            if inflexion.find('./orthography').text == orthography:
+                return inflexion.find('./grammaticalNumber').text
+    raise ValueError('Word not found')
+    
+def pluralize(orthography):
+    lexical_entry = get_lexical_entry(orthography)
+    if lexical_entry != None:
+        number = get_number(orthography)
+        gender = get_gender(orthography)
+        if number == 'plural':
+            print('Adjective already plural')
+            return None
+        if number == 'invariable':
+            print('Adjective invariable in number when {}'.format(gender))
+            return None
+        for inflexion in lexical_entry.findall('./formSet/inflectedForm'):
+            number_ = inflexion.find('./grammaticalNumber').text
+            gender_ = inflexion.find('./grammaticalGender').text
+            if number_ == 'plural' and gender_ == gender:
+                return inflexion.find('./orthography').text
+    raise ValueError('Word not found')
+          
+def singularize(orthography):
+    lexical_entry = get_lexical_entry(orthography)
+    if lexical_entry != None:
+        number = get_number(orthography)
+        gender = get_gender(orthography)
+        if number == 'singular':
+            print('Adjective already singular')
+            return None
+        if number == 'invariable':
+            print('Adjective invariable in number when {}'.format(gender))
+            return None
+        for inflexion in lexical_entry.findall('./formSet/inflectedForm'):
+            number_ = inflexion.find('./grammaticalNumber').text
+            gender_ = inflexion.find('./grammaticalGender').text
+            if number_ == 'singular' and gender_ == gender:
+                return inflexion.find('./orthography').text
+    raise ValueError('Word not found')
+    #précieux : number is invariable in masculine but not in feminine
