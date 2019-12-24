9f8b15dd71467818222cc5ba600a1d3e6bcb843b
psawa <mkbloody@gmail.com>
2019-12-24 12:22:32 +0100

    docstring

--- eval.py
+++ eval.py
@@ -4,18 +4,27 @@ from re import sub
 
 
 def main():
+    """
+    Launched on start, 
+    evaluate each file of the ./outputs folder if needed
+    """
     files = listdir('./expected_outputs')
     for file in files:
         if 'epicene_' + file  in listdir('./outputs'):
             print('evaluating file "{}"'.format(file))
-            path1 = './outputs/epicene_'+file
-            path2 = './expected_outputs/'+file
+            path1 = './expected_outputs/'+file
+            path2 = './outputs/epicene_'+file
             result = compare_files(path1, path2)
             
             print("result for file {}:  {}\n".format(file, result))
 
 
 def compare_files(path1,path2):
+    """
+    Takes two path of txt files,
+    cleans the files, 
+    return the f measurement score of the two .txt files
+    """
     with open(path1) as expected:
         expected = expected.read()
         expected = sub('(\n)+', ' ', expected)
