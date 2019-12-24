b109cc0c9f5faf1876571ea97f179d5b6c6659ca
Dodo-s95 <educa95@gmail.com>
2019-12-24 12:19:52 +0100

    files beautified

--- evaluation.py
+++ evaluation.py
@@ -6,16 +6,16 @@ from re import sub
 def main():
     files = listdir('./expected_outputs')
     for file in files:
-        if 'epicene_' + file  in listdir('./outputs'):
+        if 'epicene_' + file in listdir('./outputs'):
             print('evaluating file "{}"'.format(file))
-            path1 = './outputs/epicene_'+file
-            path2 = './expected_outputs/'+file
+            path1 = './outputs/epicene_' + file
+            path2 = './expected_outputs/' + file
             result = compare_files(path1, path2)
-            
+
             print("result for file {}:  {}\n".format(file, result))
 
 
-def compare_files(path1,path2):
+def compare_files(path1, path2):
     with open(path1) as expected:
         expected = expected.read()
         expected = sub('(\n)+', ' ', expected)
@@ -29,6 +29,7 @@ def compare_files(path1,path2):
         got = got.split(' ')
         got = set(got)
     return f_measure(expected, got)
+
 
 if __name__ == '__main__':
     main()
