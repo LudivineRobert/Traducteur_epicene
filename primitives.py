874747c8aed0aebb266373a6e6498d61f1fbf03b
Dodo-s95 <educa95@gmail.com>
2019-12-24 12:13:05 +0100

    primitives beautified

--- primitives.py
+++ primitives.py
@@ -3,8 +3,9 @@
 
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('wolf/wolf-1.0b4.xml')
-root = tree.getroot()
+tree = ET.parse('wolf/wolf-1.0b4.xml')  # Use ET to parse the xml document
+root = tree.getroot()  # Initialisation
+
 
 def get_lexical_entry(lemma):
     """
@@ -20,17 +21,19 @@ def get_lexical_entry(lemma):
                 return element
     return None
 
+
 def get_hyperonym(lexical_entry):
     """
     Lexical_entry is the adress in the tree
     Get the hyperonym of the element
     """
-    if lexical_entry != None:
-        if lexical_entry.find('./ILR[@type="hypernym"]') != None:
+    if lexical_entry is not None:
+        if lexical_entry.find('./ILR[@type="hypernym"]') is not None:
             return lexical_entry.find('./ILR[@type="hypernym"]').text
         else:
             return lexical_entry.find('./ILR[@type="instance_hypernym"]').text
     return None
+
 
 def find_primitive(ID):
     """
@@ -42,41 +45,34 @@ def find_primitive(ID):
             return element
     raise ValueError('Word not found')
 
+
 def get_from_P(ID_adress):
     '''
     ID_adress is the element in the tree corresponding to the primitive
     '''
-    if ID_adress != None:
+    if ID_adress is not None:
         return ID_adress.find('./ILR[@type="hypernym"]').text
     return None
 
-#The fonction below is now useless for the loop because of the _ENTRY_ in LITERAL we can't go from Lemma to ID every times
-def from_primitive_to_lemma(Primitive):
-    """Primitive = element with the ID of the primitive
-    get the lemma"""
-    if Primitive != None:
-        return Primitive.find('./SYNONYM/LITERAL').text
-    return None
-    #Btw, attention au différent LITERAL si il apparait en deuxième ou troisième position, il n'est pas pris en compte
 
-
-def is_human(word):
+def is_human(ID_word):
     """
     word is currently the ID of a word (yeah we should change the name of that variable)
     so we can start the loop between ID to hypernym
     """
-    current_loc = find_primitive(word)
+    current_loc = find_primitive(ID_word)
 
     identifiant = current_loc.find('./ID').text
-    #print('Emplacements',identifiant)
-    if identifiant in ('eng-30-00007846-n','eng-30-00007846-n'):
+
+    if identifiant in ('eng-30-00007846-n', 'eng-30-00007846-n'):
         return True
 
-    elif identifiant not in ('eng-30-00001740-n','eng-30-00002137-n'):
+    elif identifiant not in ('eng-30-00001740-n', 'eng-30-00002137-n'):
         identifiant = get_hyperonym(current_loc)
         return is_human(identifiant)
 
     return False
+
 
 def is_human_from_noun(noun):
     return is_human(get_hyperonym(get_lexical_entry(noun)))
