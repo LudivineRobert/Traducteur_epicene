1549fc869a9179ef4e0effeac634c99f87818cb4
psawa <mkbloody@gmail.com>
2019-12-25 16:29:20 +0100

    name change

---
+++ clean.py
@@ -0,0 +1,21 @@
+from os import listdir
+from re import sub
+
+
+def clean(path):
+    file =  open(path, 'r', encoding = 'utf-8-sig')
+    content = file.read()
+    content = sub('(\n)+', ' ', content)
+    content = sub('( )+', ' ', content)
+    content = sub('(\s)([.,])+', '\\2', content)
+    content = sub('(\s)(-)(\s)', '\\2', content)
+    content = sub('(\s)(\))', '\\2', content)
+    content = sub('(\()(\s)', '\\1', content)
+    content = sub('([\'’])\s', '’', content)
+    #print(content)
+    #breakpoint()
+    file.close()
+    file = open(path, 'w', encoding = 'utf-8-sig')
+    file.write(content)
+    file.close()
+
